42e720d20ac52cafab382db498bb5215bddc155b
dasgk <674026565@qq.com>
2018-10-31 22:25:08 +0800

    修改模板

--- mysite/mysite/controllers/templatecontroller.py
+++ mysite/mysite/controllers/templatecontroller.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render, render_to_response
+from django.template import Template,Context
 from datetime import  datetime
 def show_template(request):
-    return render_to_response('test.html', {'item': '哈哈'})
+    return render_to_response('test.html', {'person_name':'周建业'})
 
